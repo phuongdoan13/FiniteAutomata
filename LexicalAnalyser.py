6d75cfa8e56aa784ed7c63bbfdd9410e9eec4fbe
phuongdoan13 <35623720+phuongdoan13@users.noreply.github.com>
2020-08-24 14:34:15 +1000

    Add Exception description

--- LexicalAnalyser.py
+++ LexicalAnalyser.py
@@ -2,10 +2,23 @@ from enum import Enum, auto
 
 
 class NumberException(Exception):
-    
+    """
+        Numbers will be in the following formats, otherwise the program throws error:
+        1) A string of digits possibly followed by a decimal point and a non-empty string of digits. 
+        2) If there is a decimal point, the string before the decimal point should consist only of 0.
+        3)If the number starts with 0, it is either just 0, 
+        or 0.[something] where the [something] is a string of digits.
+    """
+    pass
 
 
 class ExpressionException(Exception):
+    """
+        Expressions will beee in the following formats, otherwise the program throws error:
+        1) A valid number is a valid expression. 
+        2) Any valid expression, followed by an operator, followed by a valid number.
+        3) The operators and numbers may or may not be separated by white space.
+    """
     pass
 
 
